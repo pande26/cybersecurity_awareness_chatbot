40b617a1d0acdac6dc18e1bc238a7344ce07a21c
Pandelani Munenyiwa <you@example.com>
2026-03-23 13:17:59 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cybersecurity_awareness_chatbot
+{//start of namespace
+    public class Program
+    {//start of class
+        static void Main(string[] args)
+        {//start of main method
+
+        }//end of main method
+
+    }//end of class
+
+}//end of namespace
